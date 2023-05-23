a26ec52e643b06f9465dfb1ccb8cf46908f3a887
aboyd47 <123212035+aboyd47@users.noreply.github.com>
2023-05-22 23:39:46 -0500

    updated routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,65 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({ include: [{ model: Product }] });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json({ message: 'not found!' });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, { include: [{ model: Product }] });
+    
+    if (!category) {
+      res.status(404).json({ message: 'id not found' });
+      return;
+    }
+
+  } catch (err) {
+    res.status(500).json({ message: 'not found!' });
+  }
+
 });
 
-router.post('/', (req, res) => {
   // create a new category
-});
+  router.post('/', async (req, res) => {
+    try {
+      const newCategory = await Category.create(req.body);
+      res.status(200).json(newCategory);
+    } catch (err) {
+      res.status(400).json({ message: 'creation failed' });
+    }
+  });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updated = await Category.update(req.body, { where: { id: req.params.id } });
+
+    !updated[0] ? res.status(404).json({ message: 'id not found' }) : res.status(200).json(updated);
+  } catch (err) {
+
+    res.status(500).json({ message: 'update failed' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    
+    const deleted = await Category.destroy({ where: { id: req.params.id } });
+    !deleted ? res.status(404).json({ message: 'id not found' }) : res.status(200).json(deleted);
+  } // If there is an error, send a 500 status with the error
+  catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,10 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
-
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
-
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 module.exports = router;
